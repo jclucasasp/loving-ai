1a330b447af49509563d885ea9a646bd6cc8e4fe
JC Lucas <jclucasasp@gmail.com>
2024-08-12 14:12:41 +0200

    Added chat component

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,25 @@ import Profiles from './components/profile';
 import Matches from './components/matches';
 import { MatchList } from './lib/constants';
 import { useState } from 'react';
+import ChatMessages from './components/chat';
 
 function App() {
 
-  type StateTypes = { screen: 'profile' | 'match' }
+  type StateTypes = { screen: 'profile' | 'match' | 'chat' }
 
   const [currentScreen, setCurrentScreen] = useState<StateTypes>({ screen: 'profile' });
 
   return (
     <div className='max-w-lg mx-auto mt-3'>
       <nav className='flex justify-between mb-6'>
-        
+
         <User className='cursor-pointer w-8 h-8 hover:w-9 hover:h-9' onClick={() => setCurrentScreen({ screen: 'profile' })} />
-        <div className='w-9 h-9'/>
+        <div className='w-9 h-9' />
         <MessageCircle className='cursor-pointer w-8 h-8 hover:w-9 hover:h-9' onClick={() => setCurrentScreen({ screen: 'match' })} />
       </nav>
-      {currentScreen.screen === 'profile' ? <Profiles /> : <Matches profiles={MatchList}/>}
+      {currentScreen.screen === 'profile' && <Profiles />}
+      {currentScreen.screen === 'match' && <Matches profiles={MatchList} />}
+      {currentScreen.screen === 'chat' && <ChatMessages />}
     </div>
   );
 }
---
+++ src/components/chat.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { MessagesArr } from "../lib/constants";
+
+export default function ChatMessages() {
+    const [message, setMessage] = useState<string>('');
+
+    const handleClick = () => {
+        if (message.trim()) {
+            console.log(message);
+        }
+        setMessage('');
+    }
+
+    return (
+        <div>
+            <h2 className="text-2xl font-bold mb-4 border-b-2 border-gray-300">Chat with Katerina</h2>
+            <div className="h-[50vh] overflow-y-auto">
+                {MessagesArr.map((m, i) => {
+                    return (<p key={i} className="">{m}</p>)
+                })}
+            </div>
+            <div className="flex gap-2 align-center">
+                <input
+                    type="text"
+                    placeholder="Type a message..."
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                    className="w-full p-2 mb-3 border border-gray-300 rounded-lg"
+                />
+                <button onClick={handleClick} className="rounded-lg bg-blue-500 text-white p-2 h-11 hover:shadow-lg">Send</button>
+            </div>
+        </div>
+    );
+}
