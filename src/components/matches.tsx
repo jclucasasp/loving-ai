2ea4920b843597a0ca5a9ea609b9bb22c3fffbe9
JC Lucas <jclucasasp@gmail.com>
2024-08-12 17:56:37 +0200

    Made Matches clickable

--- src/components/matches.tsx
+++ src/components/matches.tsx
@@ -11,7 +11,9 @@ export default function Matches({ profiles }: { profiles: MatchScreenProps[] }) 
             <h2 className="text-2xl font-bold border-b-2 border-gray-300">Current Matches</h2>
             {profiles.map((profile) => (
                 <li className="flex items-center gap-2" key={profile.id}>
+                    <button onClick={()=> console.log(profile)}>
                     <img src={profile.imageUrl} width={50} height={50} className="rounded-full" />
+                    </button>
                     <h3>{profile.firstName} {profile.lastName}</h3>
                 </li>
             ))}
