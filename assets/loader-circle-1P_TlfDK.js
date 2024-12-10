7081a509e09f9422cb46c7b006c5004806cb5c44
jc <jclucasasp@gmail.com>
2024-12-10 11:49:57 +0200

    Updates

---
+++ assets/loader-circle-1P_TlfDK.js
@@ -0,0 +1,6 @@
+import{F as e}from"./index-BOec_SUu.js";/**
+ * @license lucide-react v0.427.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const r=e("LoaderCircle",[["path",{d:"M21 12a9 9 0 1 1-6.219-8.56",key:"13zald"}]]);export{r as L};
