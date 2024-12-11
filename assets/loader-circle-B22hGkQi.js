25b64965f14e81045c885a5ea705ecee9b28ee89
jc <jclucasasp@gmail.com>
2024-12-11 10:42:05 +0200

    Updates

--- assets/loader-circle-B22hGkQi.js
+++ assets/loader-circle-B22hGkQi.js
@@ -1,4 +1,4 @@
-import{F as e}from"./index-Bgy2ZSMI.js";/**
+import{F as e}from"./index-BO3Okp7x.js";/**
  * @license lucide-react v0.427.0 - ISC
  *
  * This source code is licensed under the ISC license.
