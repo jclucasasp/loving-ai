cbd6a8787d6061fb7129e9236989d879446c8c0d
jc <jclucasasp@gmail.com>
2024-12-13 18:08:06 +0200

    Updates

--- assets/loader-circle-CbjjgNm6.js
+++ assets/loader-circle-CbjjgNm6.js
@@ -1,4 +1,4 @@
-import{E as e}from"./index-Bimh7iAN.js";/**
+import{E as e}from"./index-B3mHd3VY.js";/**
  * @license lucide-react v0.427.0 - ISC
  *
  * This source code is licensed under the ISC license.
