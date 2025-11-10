a83d43ac69036d354a7d00397a067217674bc3e9
jc <jclucasasp@gmail.com>
2025-11-10 19:34:51 +0200

    Updates

--- assets/loader-circle-B2l3ETOU.js
+++ assets/loader-circle-B2l3ETOU.js
@@ -1,4 +1,4 @@
-import{F as e}from"./index-pYArvi7X.js";/**
+import{F as e}from"./index-3Yo1ha65.js";/**
  * @license lucide-react v0.427.0 - ISC
  *
  * This source code is licensed under the ISC license.
