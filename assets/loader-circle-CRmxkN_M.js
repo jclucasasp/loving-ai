d694915b88289ff98f30ddf4506fc7f66a3b8f1b
jc <jclucasasp@gmail.com>
2025-11-17 14:39:37 +0200

    Updates

--- assets/loader-circle-CRmxkN_M.js
+++ assets/loader-circle-CRmxkN_M.js
@@ -1,4 +1,4 @@
-import{F as e}from"./index-3Yo1ha65.js";/**
+import{F as e}from"./index-35wuO1Kw.js";/**
  * @license lucide-react v0.427.0 - ISC
  *
  * This source code is licensed under the ISC license.
