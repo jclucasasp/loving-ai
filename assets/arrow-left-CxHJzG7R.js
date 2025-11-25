616ab6ae17a66f6a98f69392828473e95c82beba
jc <jclucasasp@gmail.com>
2025-11-25 16:45:30 +0200

    Updates

--- assets/arrow-left-CxHJzG7R.js
+++ assets/arrow-left-CxHJzG7R.js
@@ -1,4 +1,4 @@
-import{F as e}from"./index-DXCPWPkR.js";/**
+import{F as e}from"./index-V0tNZ0Rl.js";/**
  * @license lucide-react v0.427.0 - ISC
  *
  * This source code is licensed under the ISC license.
