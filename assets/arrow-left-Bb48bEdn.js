65ae7cf92906b2f9c2c44e5c6d06a2d6493a8131
jc <jclucasasp@gmail.com>
2025-12-09 15:33:33 +0200

    Updates

--- assets/arrow-left-Bb48bEdn.js
+++ assets/arrow-left-Bb48bEdn.js
@@ -1,4 +1,4 @@
-import{a1 as e}from"./index-BBk-v4uc.js";/**
+import{a1 as e}from"./index-ZMkbI5ir.js";/**
  * @license lucide-react v0.427.0 - ISC
  *
  * This source code is licensed under the ISC license.
